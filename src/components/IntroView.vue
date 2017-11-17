4a9253c10fcfa3f7d7b093df883db7c98f210074
Guillaume Gagnaire <contact@scrola.net>
2017-11-17 11:54:48 +0100

    Ajout d'infos personnelles

--- src/components/IntroView.vue
+++ src/components/IntroView.vue
@@ -3,7 +3,8 @@
         <img src="src/assets/photo.jpg" />
         Depuis mes 15 ans, et d'autant plus après mes études à <strong>EPITECH Toulouse</strong>, j'ai participé à la création de nombreuses entreprises et projets,
         en dirigeant l'aspect technique de ceux-ci.<br /><br />
-        Après ces années d'expérience en tant que freelance ou associé, je suis désormais Lead developer chez <strong>NAXXT</strong>, à <strong>Bordeaux</strong>, où je suis en charge du développement de cette <strong>Data Management Platform</strong>.
+        Après ces années d'expérience en tant que freelance ou associé, je suis désormais Lead developer chez <strong>NAXXT</strong>, à <strong>Bordeaux</strong>, où je suis en charge du développement de cette <strong>Data Management Platform</strong>.<br /><br />
+        Mes activités personnelles tournent autour du développement de <a href="https://www.github.com/42php/42php" target="_blank"><i class="fab fa-github"></i> 42php</a>, un framework PHP orienté CMS, ainsi qu'autour de la moto, la musique et l'haltérophilie.
     </article>
 </template>
 
