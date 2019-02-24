0131a0d7c64bd78c902be8242b4bc68146c83a1c
Guillaume GAGNAIRE <alduro.estallas@gmail.com>
2019-02-24 11:04:40 +0100

    Basic store

---
+++ store/cv.js
@@ -0,0 +1,139 @@
+export const state = () => ({
+    hard_skills: [
+        {
+            name: "Backend",
+            slug: "backend",
+            skills: [
+                {
+                    name: "PHP",
+                    level: "expert",
+                    slug: "php"
+                },
+                {
+                    name: "Laravel",
+                    level: "confirmed",
+                    slug: "laravel"
+                },
+                {
+                    name: "NodeJS",
+                    level: "confirmed",
+                    slug: "nodejs"
+                }
+            ]
+        },
+        {
+            name: "Frontend",
+            slug: "frontend",
+            skills: [
+                {
+                    name: "HTML / CSS",
+                    level: "expert",
+                    slug: "html-css"
+                },
+                {
+                    name: "JS",
+                    level: "expert",
+                    slug: "js"
+                },
+                {
+                    name: "Typescript",
+                    level: "confirmed",
+                    slug: "typescript"
+                },
+                {
+                    name: "Angular",
+                    level: "confirmed",
+                    slug: "angular"
+                },
+                {
+                    name: "VueJS",
+                    level: "expert",
+                    slug: "vuejs"
+                },
+                {
+                    name: "NuxtJS",
+                    level: "confirmed",
+                    slug: "vuejs"
+                }
+            ]
+        },
+        {
+            name: "Databases",
+            slug: "databases",
+            skills: [
+                {
+                    name: "MySQL",
+                    level: "expert",
+                    slug: "mysql"
+                },
+                {
+                    name: "MongoDB",
+                    level: "expert",
+                    slug: "mongodb"
+                }
+            ]
+        },
+        {
+            name: "Devops",
+            slug: "devops",
+            skills: [
+                {
+                    name: "Debian",
+                    level: "confirmed",
+                    slug: "debian"
+                },
+                {
+                    name: "Apache2",
+                    level: "confirmed",
+                    slug: "apache2"
+                },
+                {
+                    name: "NGINX",
+                    level: "confirmed",
+                    slug: "nginx"
+                },
+                {
+                    name: "AWS",
+                    level: "confirmed",
+                    slug: "aws"
+                },
+                {
+                    name: "Docker",
+                    level: "confirmed",
+                    slug: "docker"
+                }
+            ]
+        },
+        {
+            name: "Others",
+            slug: "others",
+            skills: [
+                {
+                    name: "Git",
+                    level: "confirmed",
+                    slug: "git"
+                },
+                {
+                    name: "Adobe Photoshop",
+                    level: "confirmed",
+                    slug: "photoshop"
+                }
+            ]
+        }
+    ],
+    soft_skills: [
+        "Curious",
+        "Teacher at heart",
+        "Entrepreneur mind"
+    ],
+    experiences: [],
+    other_experiences: [
+
+    ],
+    interests: [
+        "Motorcycle (road and off-road)",
+        "Music (drummer, but also guitarist and bassist)",
+        "Photography",
+        "Powerlifting"
+    ]
+});
