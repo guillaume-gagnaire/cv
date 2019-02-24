464169f70ce374aef3b3b938572e49a66b2f4d1d
Guillaume GAGNAIRE <alduro.estallas@gmail.com>
2019-02-24 17:56:31 +0100

    Page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'cv',
+    title: 'Guillaume Gagnaire - Lead developer in Bordeaux, France',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
