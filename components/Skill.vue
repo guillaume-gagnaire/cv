2be0a0c0acadf011960a6f79df2fb74a0de056b4
Guillaume Gagnaire <guillaume.gagnaire@digital-ize.com>
2019-02-25 13:53:13 +0100

    Skill component

---
+++ components/Skill.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="skill" :style="{ backgroundImage: `url(~assets/skills/${skill.slug}.png)` }">
+    <span>{{ skill.name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["skill"]
+};
+</script>
+
+<style lang="scss">
+.skill {
+  display: -ms-inline-flexbox;
+  display: -webkit-inline-flex;
+  display: inline-flex;
+  -webkit-flex-direction: row;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-align-content: stretch;
+  -ms-flex-line-pack: stretch;
+  align-content: stretch;
+  -webkit-align-items: flex-end;
+  -ms-flex-align: end;
+  align-items: flex-end;
+  height: 150px;
+  width: 150px;
+}
+</style>
+
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,18 +1,25 @@
 <template>
   <section class="container">
-    <div>
-      Resume
+    <div v-for="(cat, i) in $store.state.cv.hard_skills" :key="i">
+      <h2>{{ cat.name }}</h2>
+      <skill v-for="(skill, key) in cat.skills" :key="key" :skill="skill"></skill>
     </div>
   </section>
 </template>
 
 <script>
+import Skill from "~/components/Skill.vue";
+
 export default {
-  
-}
+  components: { Skill }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 20px;
+}
 </style>
 
