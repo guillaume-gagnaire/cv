55c42e165e6d8375aac6be775f55d731e584f9b3
Guillaume Gagnaire <guillaume.gagnaire@digital-ize.com>
2019-02-25 14:02:54 +0100

    Skill view

--- components/Skill.vue
+++ components/Skill.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="skill" :style="{ backgroundImage: `url(~assets/skills/${skill.slug}.png)` }">
-    <span>{{ skill.name }}</span>
+    <div class="hover">
+      <span>{{ skill.name }}</span>
+    </div>
   </div>
 </template>
 
@@ -27,11 +29,53 @@ export default {
   -webkit-align-content: stretch;
   -ms-flex-line-pack: stretch;
   align-content: stretch;
-  -webkit-align-items: flex-end;
-  -ms-flex-align: end;
-  align-items: flex-end;
+  -webkit-align-items: stretch;
+  -ms-flex-align: stretch;
+  align-items: stretch;
   height: 150px;
   width: 150px;
+  margin: 0 10px 10px 0;
+
+  .hover {
+    -webkit-flex: 1 1 auto;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    display: -ms-inline-flexbox;
+    display: -webkit-inline-flex;
+    display: inline-flex;
+    -webkit-flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -webkit-flex-wrap: nowrap;
+    -ms-flex-wrap: nowrap;
+    flex-wrap: nowrap;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-align-content: stretch;
+    -ms-flex-line-pack: stretch;
+    align-content: stretch;
+    -webkit-align-items: flex-end;
+    -ms-flex-align: end;
+    align-items: flex-end;
+    background: rgba(0, 0, 0, 0);
+    -webkit-transition: all 200ms ease-in-out;
+    -moz-transition: all 200ms ease-in-out;
+    -ms-transition: all 200ms ease-in-out;
+    -o-transition: all 200ms ease-in-out;
+    transition: all 200ms ease-in-out;
+    color: #333;
+    font-weight: 300;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.5);
+      color: #fff;
+    }
+
+    span {
+      padding: 5px;
+    }
+  }
 }
 </style>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,24 +6,30 @@
 </template>
 
 <script>
-  import MainHeader from '~/components/MainHeader.vue';
+import MainHeader from "~/components/MainHeader.vue";
 
-  export default {
-    components: { MainHeader }
-  }
+export default {
+  components: { MainHeader }
+};
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Cabin:300,400,600');
-  @import '@drewbot/sass-flexbox-grid/public/sass-flexbox/main.min.css';
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400|Cabin:300,400,600");
+@import "@drewbot/sass-flexbox-grid/public/sass-flexbox/main.min.css";
 
-  html, body {
-    margin: 0;
-    padding: 0;
-    font-family: "Open Sans", sans-serif;
-  }
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Open Sans", sans-serif;
+}
 
-  h1, h2, h3, h4, h5, h6 {
-    font-family: "Cabin", sans-serif;
-  }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Cabin", sans-serif;
+}
 </style>
