e0f0971c8218fd6a2c90fbefc3a31f190278ead4
Guillaume GAGNAIRE <9398610+guillaume-gagnaire@users.noreply.github.com>
2019-08-13 22:47:39 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145689749-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-145689749-1');
+    </script>
     <main-header></main-header>
     <nuxt/>
   </div>
